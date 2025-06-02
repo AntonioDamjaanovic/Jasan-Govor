60f36c037f569abd2b2f62cf0debae7d152b4de2
AntonioDamjaanovic <damjanovic1109@gmail.com>
2025-06-02 15:01:12 +0200

    Finalize the look of Home Screen

--- app/src/main/java/com/example/jasangovor/ui/HomeScreen.kt
+++ app/src/main/java/com/example/jasangovor/ui/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.example.jasangovor.ui
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -27,6 +29,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jasangovor.R
@@ -54,11 +57,37 @@ fun HomeScreen(
                 .weight(1f)
                 .padding(horizontal = 30.dp, vertical = 60.dp)
         ) {
-            HomeHeader(dayStreak = 0)
-            Spacer(modifier = Modifier.height(80.dp))
-            BrownContainer(title = "DNEVNA\nVJEŽBA", iconResId = R.drawable.ic_rocket)
+            HomeHeader(dayStreak = 0, onClick = {})
             Spacer(modifier = Modifier.height(50.dp))
-            BrownContainer(title = "BRZA\nVJEŽBA", iconResId = R.drawable.ic_microphone)
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                item {
+                    BigBrownContainer(title = "DNEVNA\nVJEŽBA", iconResId = R.drawable.ic_rocket)
+                }
+                item {
+                    Spacer(modifier = Modifier.height(30.dp))
+                }
+                item {
+                    BigBrownContainer(title = "BRZA\nVJEŽBA", iconResId = R.drawable.ic_microphone)
+                }
+                item {
+                    Spacer(modifier = Modifier.height(30.dp))
+                }
+                item {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        SmallBrownContainer(title = "DIŠI I\nOPUSTI SE", iconResId = R.drawable.ic_meditation, onClick = {})
+                        SmallBrownContainer(title = "STRAŠNI\nZVUKOVI", iconResId = R.drawable.ic_fear, onClick = {})
+                        SmallBrownContainer(title = "DNEVNA\nPROCJENA", iconResId = R.drawable.ic_graph, onClick = {})
+                    }
+                }
+            }
         }
         BlackBottomBar()
     }
@@ -66,7 +95,8 @@ fun HomeScreen(
 
 @Composable
 fun HomeHeader(
-    dayStreak: Int
+    dayStreak: Int,
+    onClick: () -> Unit
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
@@ -89,6 +119,7 @@ fun HomeHeader(
                 modifier = Modifier.size(26.dp),
                 contentScale = ContentScale.Fit
             )
+
             Spacer(modifier = Modifier.width(4.dp))
             Text(
                 text = "$dayStreak",
@@ -97,21 +128,29 @@ fun HomeHeader(
                 fontSize = 26.sp
             )
         }
-
-        Image(
-            painter = painterResource(R.drawable.ic_avatar),
-            contentDescription = "Avatar Icon",
+        Box(
             modifier = Modifier
-                .size(50.dp)
+                .size(45.dp)
                 .clip(CircleShape)
-                .background(Color(0xFFEDE4FF)),
-            contentScale = ContentScale.Crop
-        )
+                .background(ContainerColor)
+                .clickable(onClick = onClick),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_avatar),
+                contentDescription = "Avatar Icon",
+                modifier = Modifier
+                    .size(45.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xFFEDE4FF)),
+                contentScale = ContentScale.Crop
+            )
+        }
     }
 }
 
 @Composable
-fun BrownContainer(
+fun BigBrownContainer(
     title: String,
     @DrawableRes iconResId: Int
 ) {
@@ -152,6 +191,45 @@ fun BrownContainer(
             horizontalArrangement = Arrangement.Center,
         ) {
             StartExerciseButton()
+        }
+    }
+}
+
+@Composable
+fun SmallBrownContainer(
+    title: String,
+    @DrawableRes iconResId: Int,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .height(120.dp)
+            .width(100.dp)
+            .clip(RoundedCornerShape(24.dp))
+            .background(color = ContainerColor)
+            .clickable(onClick = onClick),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                painter = painterResource(id = iconResId),
+                contentDescription = "Context Icon",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(40.dp)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = title,
+                color = TitleColor,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 12.sp,
+                lineHeight = 14.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.width(80.dp)
+            )
         }
     }
 }
