6b9a641b63303d9be8fc14f83b00fd7ed98bc8cc
AntonioDamjaanovic <damjanovic1109@gmail.com>
2025-06-20 20:39:46 +0200

    Add RecordingsListScreen

---
+++ app/src/main/java/com/example/jasangovor/ui/RecordingsListScreen.kt
@@ -0,0 +1,177 @@
+package com.example.jasangovor.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.jasangovor.R
+import com.example.jasangovor.Routes
+import com.example.jasangovor.ui.theme.BackgroundColor
+import com.example.jasangovor.ui.theme.ContainerColor
+import java.io.File
+
+@Composable
+fun RecordingsListScreen(
+    navigation: NavController,
+    audioFiles: List<File>,
+    onPlay: (File) -> Unit,
+    onStop: () -> Unit
+) {
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = BackgroundColor)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            RecordingsListHeader(
+                navigation = navigation,
+                title = "Vaši snimci"
+            )
+            LazyColumn(modifier = Modifier.padding(30.dp)) {
+                items(audioFiles) { file ->
+                    RecordingContainer(
+                        title = file.name,
+                        audioFile = file,
+                        onPlay = onPlay,
+                        onStop = onStop
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                }
+            }
+        }
+        BlackBottomBar()
+    }
+}
+
+@Composable
+fun RecordingsListHeader(
+    navigation: NavController,
+    title: String
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(130.dp)
+            .background(ContainerColor)
+            .padding(20.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            IconButton(onClick = { navigation.popBackStack(Routes.SCREEN_RECORD_VOICE, false) }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_backarrow),
+                    contentDescription = "Back Arrow",
+                    modifier = Modifier.size(45.dp),
+                    tint = Color.Black
+                )
+            }
+            Text(
+                text = title,
+                color = Color.White,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 24.sp,
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Center
+            )
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_info),
+                    contentDescription = "Info Icon",
+                    modifier = Modifier.size(40.dp),
+                    tint = Color.Black
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun RecordingContainer(
+    title: String,
+    audioFile: File,
+    onPlay: (File) -> Unit,
+    onStop: () -> Unit
+) {
+    var isPlaying by remember { mutableStateOf(false) }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth()
+            .background(color = ContainerColor)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+            Text(
+                text = title,
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.weight(1f)
+            )
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .background(Color.White)
+                    .clickable(
+                        onClick = {
+                            isPlaying = !isPlaying
+                            if(isPlaying) {
+                                onPlay(audioFile)
+                            } else {
+                                onStop()
+                            }
+                        }
+                    )
+            ) {
+                val iconRes = if (isPlaying) R.drawable.ic_stop else R.drawable.ic_play
+                Image(
+                    painter = painterResource(id = iconRes),
+                    contentDescription = if (isPlaying) "Stop Playing Icon" else "Play Icon",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+        }
+    }
+}
